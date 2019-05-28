bd43a5551392cd60968943aa00272b121d0b4ba3
Joana Cazaurang <joana.2000@free.fr>
2019-05-28 10:12:29 +0200

    TestView creation

--- boulderDash/view/testView/view/ViewTest.java
+++ boulderDash/view/testView/view/ViewTest.java
@@ -32,23 +32,9 @@ public class ViewTest {
 	}
 
 	@Test
-	public void testKeyCodeToControllerOrder() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testPrintMessage() {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	public void testRun() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetController() {
-		fail("Not yet implemented");
-	}
 
 }
