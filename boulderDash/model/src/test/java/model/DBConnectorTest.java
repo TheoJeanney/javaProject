495990a0f57631f0f431fba955cf63c26891ba9e
Jeanney <theo.jeanney@orange.fr>
2019-05-28 15:47:16 +0200

    boom why vincent

---
+++ boulderDash/model/src/test/java/model/DBConnectorTest.java
@@ -0,0 +1,64 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DBConnectorTest {
+	
+	
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDBConnector() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsOpen() {
+		fail("Not yet implemented");
+	}
+
+
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+
+	@Test
+	public void testGetConnection() {
+		fail("Not yet implemented");
+	}
+
+
+	@Test
+	public void testGetStatement() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExecuteQuery() {
+		fail("Not yet implemented");
+	}
+
+}
