705a234fea98eb5e2afc119b2808c8ae63bdbba1
Jeanney <theo.jeanney@orange.fr>
2019-05-30 22:57:10 +0200

    entityTest

--- boulderDash/entity/src/test/java/entity/EntityTest.java
+++ boulderDash/entity/src/test/java/entity/EntityTest.java
@@ -12,8 +12,9 @@ import org.junit.Test;
 
 public class EntityTest {
 	
-	private Entity entity;
+	
 	private Point testPoint = new Point (1,1);
+	private Entity entity = new Dirt(testPoint);
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -38,22 +39,20 @@ public class EntityTest {
 		final double expectedX = 1.0;
 		final double expectedY = 1.0;
 		
-		//System.out.println("x : " + entity.getPosition().getX());
-		//System.out.println("y : " + entity.getPosition().getY());
+		System.out.println("x : " + entity.getPosition().getX());
+		System.out.println("y : " + entity.getPosition().getY());
 		
-		//assertEquals(expectedX,entity.getPosition().getX(),0);
-		//assertEquals(expectedY, entity.getPosition().getY(),0);
+		assertEquals(expectedX,entity.getPosition().getX(),0);
+		assertEquals(expectedY, entity.getPosition().getY(),0);
 		
 	}
 
-	@SuppressWarnings("null")
 	@Test
 	public void testGetAttribute() {
 		final boolean expected = true;
-		final Entity entity = null;
 		
-		//System.out.println("test : "+ entity.getAttribute(Attribute.breakable));
-		//assertEquals(expected, entity.getAttribute(Attribute.breakable));
+		System.out.println("test : "+ entity.getAttribute(Attribute.breakable));
+		assertEquals(expected, entity.getAttribute(Attribute.breakable));
 		
 	}
 
